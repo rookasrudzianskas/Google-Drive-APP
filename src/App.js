47b49d18a8b3334e0c07a113f7c81783f5b0105b
Rokas <rookas.rudzenskas@gmail.com>
2021-06-21 17:04:21 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
-      Hello 🚀
+      <Header />
     </div>
   );
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,13 @@ const Header = () => {
     return (
         <div className="header">
             <div className="header__logo">
-                <div className="header__searchContainer"></div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/1200px-Google_Drive_icon_%282020%29.svg.png" alt=""/>
+                <span>Drive</span>
+                </div>
+                <div className="header__searchContainer">
+                    <div className="header__searchContainer"></div>
+                    <div className="header__icons"></div>
+
             </div>
         </div>
     );
