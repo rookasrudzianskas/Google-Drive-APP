a54e7db6d24cacd864caeb3c7a73e49eb41b24c4
Rokas <rookas.rudzenskas@gmail.com>
2021-06-21 16:58:45 +0300

    Rokas

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="header">
+            <div className="header__logo">
+                <div className="header__searchContainer"></div>
+            </div>
         </div>
     );
 };
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDS0lgR1mDc6XgF55bhnfpsaDnSgnuLbtE",
+    authDomain: "rookas-drive-app.firebaseapp.com",
+    projectId: "rookas-drive-app",
+    storageBucket: "rookas-drive-app.appspot.com",
+    messagingSenderId: "609463869730",
+    appId: "1:609463869730:web:c534791936eeb84f29857b",
+    measurementId: "G-QCJDC3KZ0S"
+};
