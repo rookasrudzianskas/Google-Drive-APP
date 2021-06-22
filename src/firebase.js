97d7c8d5e26840dc7ead8a94ba38544d12858ba7
Rokas <rookas.rudzenskas@gmail.com>
2021-06-22 10:06:47 +0300

    Rokas

--- src/firebase.js
+++ src/firebase.js
@@ -11,3 +11,11 @@ const firebaseConfig = {
     measurementId: "G-QCJDC3KZ0S"
 };
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+const db = firebaseApp.firestore();
+
+export {auth, provider, db, storage};
