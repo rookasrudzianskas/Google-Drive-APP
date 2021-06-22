bcb8b823edd1787d87546875b34c6f48457927be
Rokas <rookas.rudzenskas@gmail.com>
2021-06-22 11:44:14 +0300

    Roka

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import Header from "./components/header";
 import Sidebar from "./components/sidebar";
 import FilesView from "./components/filesView/FilesView";
 import {useState} from "react";
+import SideIcons from "../src/components/sideIcons/index";
 
 function App() {
 
-  const [user, setUser] = useState({
-    displayName: "Rokas Rudzianskas",
-    email: "rokas.rudzenskas@gmail.com",
-    emailVerified: true,
-    phoneNumber: null,
-    photoUrl: "https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg",
-  })
+  const [user, setUser] = useState({});
+
   return (
     <div className="app">
       <Header userPhoto={user.photoUrl} />
         <div className="app__main">
           <Sidebar />
           <FilesView />
+          <SideIcons />
         </div>
     </div>
   );
