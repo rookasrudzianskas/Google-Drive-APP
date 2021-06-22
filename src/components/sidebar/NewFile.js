1b251d91ff9848d2c357e1c915f29be01cdbc5f2
Rokas <rookas.rudzenskas@gmail.com>
2021-06-22 10:12:46 +0300

    Rokas

--- src/components/sidebar/NewFile.js
+++ src/components/sidebar/NewFile.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {AddOutlined} from "@material-ui/icons";
 import "../../styles/NewFile.css";
 import {storage, db} from "../../firebase";
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+
+function getModalStyle() {
+    return {
+        top: `50%`,
+        left: `50%`,
+        transform: `translate(-50%, -50%)`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
 
 const NewFile = () => {
+    const classes = useStyles();
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+    const [file, setFile] = useState(null);
+    const [uploading, setUploading] = useState(false);
+
     return (
         <div className="newFile">
             <div className="newFile__container">
