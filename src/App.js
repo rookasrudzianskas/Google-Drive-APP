ac94d8a70459e9b72d6d17a32f9150a009fa86b5
Rokas <rookas.rudzenskas@gmail.com>
2021-06-22 09:25:19 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app__main">
       <Header />
     </div>
   );
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import {ExpandMore, HelpOutlined, SearchOutlined, SettingsOutlined} from "@material-ui/icons";
+import {AppsOutlined, ExpandMore, HelpOutlined, SearchOutlined, SettingsOutlined} from "@material-ui/icons";
 import "../../styles/Header.css";
 
 const Header = () => {
     return (
-        <div className="header">
-            <div className="header__logo">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/1200px-Google_Drive_icon_%282020%29.svg.png" alt=""/>
-                <span>Drive</span>
-                </div>
-                <div className="header__searchContainer">
-                        <div className="header__searchBar">
-                            <SearchOutlined />
-                            <input type="text" placeholder="Search in drive"/>
-                            <ExpandMore />
-                        </div>
-                    </div>
-                    <div className="header__icons">
-                        <span>
-                            <HelpOutlined />
-                            <SettingsOutlined />
-                        </span>
 
-                        <img src="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg" alt="User Photo"/>
-                    </div>
+    <div className='header'>
+        <div className="header__logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/1200px-Google_Drive_icon_%282020%29.svg.png" alt="Google Drive" />
+            <span>Drive</span>
         </div>
+        <div className="header__searchContainer">
+            <div className="header__searchBar">
+                <SearchOutlined />
+                <input type="text" placeholder='Search in Drive' />
+                <ExpandMore />
+            </div>
+        </div>
+        <div className="header__icons">
+                <span>
+                       <HelpOutlined />
+                            <SettingsOutlined />
+                </span>
+            <AppsOutlined />
+            <img src="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg" alt="User Photo"/>
+        </div>
+    </div>
     );
 };
 
