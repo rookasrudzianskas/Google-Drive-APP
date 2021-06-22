e23608607a46c1cbcd6b91f515b86b8e5dc13ea5
Rokas <rookas.rudzenskas@gmail.com>
2021-06-22 10:02:24 +0300

    Rokas

--- src/components/filesView/FilesView.js
+++ src/components/filesView/FilesView.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const FilesView = () => {
     return (
-        <div className="filesView">
+        <div className="fileView">
+            <div className="fileView__row">
+            {/*    file cards */}
+            </div>
 
+            <div className="filesView__titles">
+                <div className="fileView__titles--left">
+                    <p>Name</p>
+                </div>
+
+                <div className="fileView__titles--right">
+                    <p>Last modified</p>
+                    <p>Files size</p>
+                </div>
+            </div>
+
+        {/*    file items go in here*/}
         </div>
     );
 };
