2cb2d4b47a99b292a50cf79415edf294b509dae3
Rokas <rookas.rudzenskas@gmail.com>
2021-06-22 10:44:12 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
+import FilesView from "./components/filesView/FilesView";
 
 function App() {
   return (
     <div className="app__main">
       <Header />
         <Sidebar />
+        <FilesView />
     </div>
   );
 }
